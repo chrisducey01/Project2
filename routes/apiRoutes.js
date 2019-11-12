fa84aa0a8c58766e5794539e85c5b66d9a021da4
Christopher Ducey <chrisducey01@me.com>
2019-11-11 22:03:10 -0700

    Updated apiRoutes.js to fix eslint error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,57 +1,63 @@
 var db = require("../models");
 var passport = require("../config/passport");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/api/examples", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Sign-up a new user
-  app.post("/api/signup", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    const family = req.body.family;
-    const role = req.body.role;
+  app.post("/api/signup", function(req, res) {
+    username = req.body.username;
+    password = req.body.password;
+    family = req.body.family;
+    role = req.body.role;
 
     if (!username) {
-      return res.status(400).json({ message: "username not passed in on request." });
+      return res
+        .status(400)
+        .json({ message: "username not passed in on request." });
     }
     if (!password) {
-      return res.status(400).json({ message: "password not passed in on request." });
+      return res
+        .status(400)
+        .json({ message: "password not passed in on request." });
     }
     if (!family) {
-      return res.status(400).json({ message: "family not passed in on request." });
+      return res
+        .status(400)
+        .json({ message: "family not passed in on request." });
     }
     if (!role) {
-      return res.status(400).json({ message: "role not passed in on request." });
+      return res
+        .status(400)
+        .json({ message: "role not passed in on request." });
     }
 
     //First create the new family and retrieve the family id
-    db.Family.create({ name: family }).
-      then(function (family) {
-        //Then create the new user
-        db.User.create({
-          name: username,
-          password: password,
-          role: role,
-          FamilyId: family.id
-        }).
-          then(function (user) {
-            res.json({ id: user.id });
-          });
+    db.Family.create({ name: family }).then(function(family) {
+      //Then create the new user
+      db.User.create({
+        name: username,
+        password: password,
+        role: role,
+        FamilyId: family.id
+      }).then(function(user) {
+        res.json({ id: user.id });
       });
+    });
   });
 
   // Login an existing user
-  app.post("/api/login", passport.authenticate("local"), function (req, res) {
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
     res.json(req.user);
   });
 
   // Route for logging user out
-  app.get("/api/logout", function (req, res) {
+  app.get("/api/logout", function(req, res) {
     if (!req.user) {
       return res.status(401).end();
     }
@@ -60,8 +66,8 @@ module.exports = function (app) {
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function (req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function (
+  app.delete("/api/examples/:id", function(req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
       res.json(dbExample);
